f7795c83c12b0f39d2e9e45151049609aaff5a9f
Thành Long <longthanh937@gmail.com>
2023-11-01 20:47:26 +0700

    first

---
+++ database/migrations/2023_11_01_120621_create_products_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id();
+            $table->string('name', 50);
+            $table->string('color', 20);
+            $table->string('brand', 50);
+            $table->interger('price');
+            $table->interger('inventory');
+            $table->string('image')->nullable();
+            $table->foreignId('category_id')->constrained();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('products');
+    }
+};
